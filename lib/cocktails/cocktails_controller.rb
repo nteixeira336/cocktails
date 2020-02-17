76d3a44f1f9053e8a1864989c73c9b54a1887c01
Jonathan <JT@Jonathans-MacBook-Air.local>
2020-02-17 11:39:16 -0500

    created a method make_cocktails that scrapes the cocktails page and puts the cocktails in an array. Then creates an instance for each cocktail.

--- lib/cocktails/cocktails_controller.rb
+++ lib/cocktails/cocktails_controller.rb
@@ -1,12 +1,18 @@
-require 'pry'
-
 class Cocktails::CocktailsController
 
   def run
-    puts "Welcome to the Cocktails CLI App!"
-    
+    make_cocktails
+    greeting
   end
 
+  def make_cocktails
+    cocktails_array = Scraper.scrape_page
+    Cocktails.create_from_collection(cocktails_array)
+  end
+
+  def greeting
+    puts "Welcome to the cocktails app!"
+  end
 
 
 
