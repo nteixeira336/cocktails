00d620dcfc8198d0c1311f21744b8ff94364bcca
Jonathan <JT@Jonathans-MacBook-Air.local>
2020-02-18 18:00:39 -0500

    created user_input method that takes user input of a number to get the description of a cocktail. Called display_cocktails to display the info. Also gave option for exiting app and incorrect input.

--- lib/cocktails/cocktails_controller.rb
+++ lib/cocktails/cocktails_controller.rb
@@ -3,10 +3,11 @@ class Cocktails::CocktailsController
   def run
     make_cocktails
     greeting
+    user_input
   end
 
   def make_cocktails
-    cocktails_array = Scraper.scrape_page
+    cocktails_array = Cocktails::Scraper.scrape_page
     Cocktails.create_from_collection(cocktails_array)
   end
 
@@ -17,7 +18,23 @@ class Cocktails::CocktailsController
   def display_cocktails
     Cocktail.all.each.with_index(1){|cocktail,index|
     puts "#{index}. #{cocktail.name}"}
-  end 
+  end
+
+  def user_input
+    input= nil
+    while input != "exit"
+      input= gets.chomp
+      if input.downcase == "cocktails"
+        puts "To see the cocktail description, please enter the number."
+        display_cocktails
+        find_cocktail_by_number
+      elsif input.downcase== "exit"
+        exit_app
+      else
+        enter_correct_input
+      end
+    end
+  end
 
 
 
