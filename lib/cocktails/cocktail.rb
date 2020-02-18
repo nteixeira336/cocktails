94c4e606ce04c61f6be2325538e53edc22aa7ede
Jonathan <JT@Jonathans-MacBook-Air.local>
2020-02-18 10:18:13 -0500

    created the create_from_collection class method that uses the scraper class to create new cocktails with name and description.

--- lib/cocktails/cocktail.rb
+++ lib/cocktails/cocktail.rb
@@ -4,9 +4,17 @@ attr_accessor :name, :description
 
 @@all= []
 
-def initialize(cocktail_hash)
-  cocktail_hash.each do |key,value|
-    self.send "#{key}=",value
+  def initialize(cocktail_hash)
+    cocktail_hash.each do |key,value|
+      self.send "#{key}=",value
+    end
+    @@all << self
   end
-  @@all << self
+
+  def self.create_from_collection(cocktails_array)
+    cocktails_array.each do |cocktail|
+      self.new(cocktail)
+    end
+  end 
+
 end
